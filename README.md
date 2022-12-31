25070ae299db492bf76fcc98457aeb0284423530
FLzyy <flzyy72@gmail.com>
2022-12-31 09:06:43 +0800

    update readme npm & optimize build.ts

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # larser
 
 Lightweight command line argument parser.
+
 Please refer to the [GitHub README](https://github.com/flzyy/larser#readme) for full documentation.
--- scripts/build.ts
+++ scripts/build.ts
@@ -1,33 +1,27 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { copyFile, mkdir, readFile, writeFile } from "fs/promises";
 import { existsSync } from "fs";
-import { dirname, join, resolve } from "path";
 
-const __dirname = (() => {
-  const x = dirname(decodeURI(new URL(import.meta.url).pathname));
-  return resolve(process.platform === "win32" ? x.substring(1) : x);
-})();
-
-const source = await readFile(join(__dirname, "../../package.json"), "utf-8");
+const source = await readFile("./package.json", "utf-8");
 const object = JSON.parse(source);
 object.scripts = {};
 object.devDependencies = {};
 
 await writeFile(
-  join(__dirname, "../../dist/package.json"),
+  "./dist/package.json",
   Buffer.from(JSON.stringify(object), "utf-8"),
 );
 
-if (!existsSync(join(__dirname, "../../dist/types"))) {
-  await mkdir(join(__dirname, "../../dist/types"));
+if (!existsSync("./dist/types")) {
+  await mkdir("./dist/types");
 }
 
 await writeFile(
-  join(__dirname, "../../dist/types/index.d.ts"),
-  await readFile(join(__dirname, "../../src/types/index.d.ts"), "utf-8"),
+  "./dist/types/index.d.ts",
+  await readFile("./src/types/index.d.ts", "utf-8"),
 );
 
 await copyFile(
-  join(__dirname, "../../README.md"),
-  join(__dirname, "../../dist/README.md"),
+  "./README.md",
+  "./dist/README.md",
 );
