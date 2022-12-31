0b28d90496a2f11e3042a3dacd30cdf175605cfc
FLzyy <flzyy72@gmail.com>
2022-12-31 09:45:43 +0800

    benchmarking

--- src/dev.ts
+++ src/dev.ts
@@ -1,12 +1,34 @@
 import larser from "./index.js";
 
-const argv = process.argv;
-const parsed = larser(argv, {
+const argv = { ...process.argv, b: "notdefault" };
+
+console.time("defaults");
+larser(argv, {
   defaults: {
     t: true,
     b: "defaulted",
   },
 });
+console.timeEnd("defaults");
 
-console.log(argv);
-console.log(parsed);
+console.time("aliased");
+larser(argv, {
+  aliases: {
+    t: ["test", "tI"],
+    b: ["b2", "be1"],
+  },
+});
+console.timeEnd("aliased");
+
+console.time("both");
+larser(argv, {
+  aliases: {
+    t: ["test", "tI"],
+    b: ["b2", "be1"],
+  },
+  defaults: {
+    t: true,
+    b: "defaulted",
+  },
+});
+console.timeEnd("both");
