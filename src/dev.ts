c556111d7fb26fb78ec3761512ad5f766dc04c9b
FLzyy <flzyy72@gmail.com>
2022-12-31 10:01:21 +0800

    micro optimizations

--- src/dev.ts
+++ src/dev.ts
@@ -1,34 +1,12 @@
 import larser from "./index.js";
 
-const argv = { ...process.argv, b: "notdefault" };
-
-console.time("defaults");
-larser(argv, {
+const argv = process.argv;
+const parsed = larser(argv, {
   defaults: {
     t: true,
     b: "defaulted",
   },
 });
-console.timeEnd("defaults");
 
-console.time("aliased");
-larser(argv, {
-  aliases: {
-    t: ["test", "tI"],
-    b: ["b2", "be1"],
-  },
-});
-console.timeEnd("aliased");
-
-console.time("both");
-larser(argv, {
-  aliases: {
-    t: ["test", "tI"],
-    b: ["b2", "be1"],
-  },
-  defaults: {
-    t: true,
-    b: "defaulted",
-  },
-});
-console.timeEnd("both");
+console.log(argv);
+console.log(parsed);
--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,22 @@ export default (
     defaults: {},
   },
 ): Parsed => {
-  console.log(options);
   const { aliases, defaults } = options;
   let parsed: Parsed = {
     _: [],
     ...defaults,
   };
 
-  const length = argv.length;
-  for (let i = 2; i < length; i++) {
+  for (let i = 2; i < argv.length; i++) {
     const cIndex = argv[i];
 
     if (cIndex.startsWith("--")) {
       const split = cIndex.trim().slice(2).split("=");
 
       if (aliases != null) {
+        let main: string[] = [];
         if (Object.keys(aliases).includes(split[0])) {
-          const main = aliases[split[0]];
-          const length = main.length;
-          for (let i = 0; i < length; i++) {
-            parsed[main[i]] = split[1];
-          }
-          parsed[split[0]] = split[1];
+          main = aliases[split[0]];
         } else if (Object.values(aliases).flat().includes(split[0])) {
           const mainstr = Object.keys(aliases).filter((k) =>
             aliases[k].includes(split[0])
@@ -36,23 +30,23 @@ export default (
           if (mainstr.length > 1) {
             throw new Error("Duplicate Aliases");
           }
-          const main = aliases[mainstr[0]];
+          main = aliases[mainstr[0]];
           parsed[mainstr[0]] = split[1];
-          const length = main.length;
-          for (let i = 0; i < length; i++) {
-            parsed[main[i]] = split[1];
-          }
         }
-      } else {
-        parsed[split[0]] = split[1];
+        for (let i = 0; i < main.length; i++) {
+          parsed[main[i]] = split[1];
+        }
       }
+
+      parsed[split[0]] = split[1];
     } else if (cIndex.startsWith("-")) {
-      const split = cIndex.trim().slice(1).split("");
-      const object = split.reduce((m, v) => {
-        m[v] = true;
-        return (m);
-      }, {});
-      parsed = { ...parsed, ...object };
+      parsed = {
+        ...parsed,
+        ...Object.assign(
+          {},
+          ...cIndex.trim().slice(1).split("").map((k) => ({ [k]: true })),
+        ),
+      };
     } else {
       parsed._.push(cIndex);
     }
