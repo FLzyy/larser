54973330d4f3676261369de4406d5b219c5c3329
FLzyy <flzyy72@gmail.com>
2023-01-23 10:15:54 +0800

    create benchmarks

---
+++ benchmarks/index.js
@@ -0,0 +1,63 @@
+import benchmark from "benchmark";
+import mri from "mri";
+import larser from "larser";
+import nopt from "nopt";
+import yargs from "yargs-parser";
+import minimist from "minimist";
+
+console.clear();
+const args = [
+  "C:\\Program Files\\nodejs\\node.exe",
+  "C:\\larser\\tests\\index.test.js",
+  "--bing=bong",
+  "--this=that",
+  "--tqa",
+  "-zyxa",
+  "-ghjk",
+  "AXY",
+  "YXA",
+  "ansdjnj",
+  "asnidqniw",
+  "--aksnd=qnieasd",
+  "--qwe=adf",
+  "--xzc=thsd2at",
+  "--asdw",
+  "-zxcaw",
+  "-qwegsc",
+  "asdqwr",
+  "ABFHUBZ",
+  "qwhihuhasd",
+  "qnwdksdqweubUBVBUha",
+  "--nasbdwBHGB=abdjNUSBduqbn",
+  "--adbjqwasd=qwdnjiansdwqwe",
+  "--qwemldaskdjn=asdnqjwenj",
+  "--wmwqiknw",
+  "-nauqwbeyugbaf",
+  "-bhvbashidvbpasd",
+  "BNJBHFJ",
+  "AIUIQ",
+  "BHDFbhashgdw",
+  "QNCJNSDZ",
+  "--ABHUJFBJA=knasjdba",
+  "--ABDHJbjq)=JBSJDaksd",
+  "--bjzxc=THVAGHF",
+  "--asldjnkjnFS",
+  "-AnjjdnNJD",
+  "-ASNQWNJsdadwKAnfn",
+  "FJANDJWjiidbuwbuBUDBhj",
+  "NDnnasndjwBASYHbhqwsa",
+  "udjbahsdBWJU",
+  "JBASJDabshdwqw",
+  "--asdhvqwhbkasd=bjsdhvbqkwefvas",
+];
+console.log(`Benchmark with ${args.length} arguments:`);
+const bench = new benchmark.Suite();
+
+bench
+  .add("minimist     ", () => minimist(args))
+  .add("larser       ", () => larser(args))
+  .add("mri          ", () => mri(args))
+  .add("nopt         ", () => nopt(args))
+  .add("yargs-parser ", () => yargs(args))
+  .on("cycle", (e) => console.log(String(e.target)))
+  .run();
